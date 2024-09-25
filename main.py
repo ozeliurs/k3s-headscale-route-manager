51a9661e6c317c74f77d9be839a56657a0372ff1
Ozeliurs <ozeliurs@gmail.com>
2024-09-25 21:20:27 +0200

    refactor(main.py): migrate from print statements to logging for better log management and flexibility

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import os
 import time
+import logging
 
 import requests
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 api_url = os.getenv('API_URL')
 api_key = os.getenv('API_KEY')
 loop_interval = int(os.getenv('LOOP_INTERVAL', 60))
 
 if not api_url or not api_key:
-    print("Please set the API_URL and API_KEY environment variables.")
+    logger.error("Please set the API_URL and API_KEY environment variables.")
     exit(1)
 
 if not api_url.startswith("http://") and not api_url.startswith("https://"):
-    print("[!] No protocol found in API_URL, using default: https://")
+    logger.warning("No protocol found in API_URL, using default: https://")
     api_url = "https://" + api_url
 
 if api_url.endswith("/"):
@@ -21,7 +25,7 @@ if api_url.endswith("/"):
 if not api_url.endswith("/api/v1"):
     api_url += "/api/v1"
 
-print(f"[D] Using API_URL: {api_url}")
+logger.debug(f"Using API_URL: {api_url}")
 
 headers = {
     'Authorization': 'Bearer ' + api_key,
@@ -33,14 +37,14 @@ while True:
     start_time = time.time()
     for route in response.json()['routes']:
         if not route["enabled"]:
-            print("Warning: Route " + route["id"] + " is disabled")
+            logger.warning(f"Route {route['id']} is disabled")
             response = requests.post(api_url + '/routes/' + route["id"] + '/enable', headers=headers)
             if response.status_code == 200:
-                print("Route " + route["id"] + " enabled")
+                logger.info(f"Route {route['id']} enabled")
             else:
-                print(f"Failed to enable route {route['id']}")
-    print("OK")
+                logger.error(f"Failed to enable route {route['id']}")
+    logger.info("OK")
 
     remaining = loop_interval - (time.time() - start_time) % loop_interval
-    print(f"Sleeping for {remaining} seconds")
+    logger.info(f"Sleeping for {remaining} seconds")
     time.sleep(remaining)
